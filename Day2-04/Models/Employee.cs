ce5094d133c9015135e42bb4a6bb22bf8550c8f5
Lovish57 <99329447+Lovish57@users.noreply.github.com>
2022-07-27 12:03:06 +0530

    Add files via upload

---
+++ Day2-04/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day2_03.Models
+{
+    public class Employee
+    {   
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+        public string Job { get; set; }
+        public int Sal { get; set; }
+        public int Deptno { get; set; }
+
+    }
+}
---
+++ Day2-04/Models/EmployeeManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Day2_03.Models
+{
+    public class EmployeeManager
+    {
+        public static List<Employee> employee = new List<Employee>();
+        public EmployeeManager()
+        {
+            employee = new List<Employee>() {
+                new Employee(){ EmployeeId = 1, EmployeeName = "Phil Jones", Job = "Sde", Sal = 5000 ,Deptno=1},
+                new Employee(){ EmployeeId = 2, EmployeeName = "Matic", Job = "accountant", Sal = 3000,Deptno=1 },
+                new Employee(){ EmployeeId = 3, EmployeeName = "Maguire", Job = "Salesman", Sal = 40000,Deptno=1 },
+                new Employee(){ EmployeeId = 4, EmployeeName = "Wan Bissaka", Job = "manager", Sal = 2000,Deptno=1 },
+                new Employee(){ EmployeeId = 5, EmployeeName = "Shaw", Job = "IT", Sal = 60000,Deptno=1 }
+            };
+        }
+        public List<Employee> GetAllDets()
+        {
+            return employee;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            return employee.Find(item => item.EmployeeId == id);
+        }
+        public void AddEmployee(Employee obj)
+        {
+            employee.Add(obj);
+        }
+        public void DeleteEmployee(int id)
+        {
+            Employee obj = employee.Find(item => item.EmployeeId == id);
+            employee.Remove(obj);
+        }
+        public void UpdateEmployee(Employee updateObj)
+        {
+            Employee obj = employee.Find(item => item.EmployeeId == updateObj.EmployeeId);
+            employee.Remove(obj);
+            employee.Add(updateObj);
+        }
+
+    }
+}
