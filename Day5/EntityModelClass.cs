f1c7f11837a0b65a60c6e8aece740ccd13ae81a6
Lovish57 <99329447+Lovish57@users.noreply.github.com>
2022-07-29 16:18:54 +0530

    Add files via upload

---
+++ Day5/EntityModelClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day5_01.Model
+{
+    public class Employee
+    {
+        public int Employeeid { get; set; }
+        public string Employee_Name { get; set; }
+        public string Job { get; set; }
+        public int Sal { get; set; }
+        public int Deptno { get; set; }
+
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
